1d2032c188e42104cdbc601108b194f1b46af51f
Chris Mendez <mendecj812@gmail.com>
2023-09-06 22:19:23 -0500

    here lies my hopes and dreams

--- script.js
+++ script.js
@@ -3,55 +3,110 @@
 // user interaction
 // update data
 // render data
-const cells = Array.from(document.querySelectorAll('.cell'));
+// const cells = Array.from(document.querySelectorAll('.cell'));
 
 const gameboard = (() => {
-  // Methods to update and query the gameboard statea
+  // Methods to update and query the gameboard state
 
-  const gameboard = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
-  // HORIZONTAL WINS
-  // 0, 1, 2
-  // gameboard.gameboard[0], gameboard.gameboard[1], gameboard.gameboard[2] === val, val, val
-  // 3, 4, 5
-  // 6, 7, 8
+  const gameboard = Array(9).fill(null);
+  // const gameboard = Array(9).fill('hi');
 
-  // VERTICAL WINS
-  // 0, 3, 6
-  // 1, 4, 7
-  // 2, 5, 8
+  const placeMark = function (index, player) {
+    const { mark } = player;
+    gameboard[index] = mark;
+  };
 
-  // DIAGONAL WINS
-  // 0, 4, 8
-  // 2, 4, 6
+  const getBoard = () => gameboard;
 
-  // const gameboard = ['', '', '', '', '', '', '', '', ''];
-  return { gameboard };
+  const printBoard = () => console.log(gameboard);
+
+  return { gameboard, placeMark, getBoard, printBoard };
 })();
 
 const gameController = (() => {
   // Methods for game logic
-  // const currentPlayer =
+
+  const WIN_COMBOS = [
+    // HORIZONTAL WINS
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+
+    // VERTICAL WINS
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+
+    // DIAGONAL WINS
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  // This will be the method of getting the entire board that our
+  // UI will eventually need to render it.
+
+  const player = (name, mark) => {
+    return { name, mark };
+  };
+
+  const playerOne = player('chris', 'X');
+  const playerTwo = player('tim', 'Y');
+
+  let activePlayer = playerOne;
+
+  const getActivePlayer = () => activePlayer;
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
+  };
+
+  const printNewRound = () => {
+    gameboard.printBoard();
+    console.log(`${activePlayer.name}'s turn.`);
+  };
+
+  const checkIfWinner = function (player) {
+    const { mark } = player;
+    const board = gameboard.getBoard();
+    console.log({ board });
+    return WIN_COMBOS.some(combo =>
+      combo.every(index => {
+        return board[index] === mark;
+      })
+    );
+  };
+
+  const printWinner = function (activePlayer) {
+    console.log(`${activePlayer.name} has won!!!`);
+  };
+
+  const playRound = function (player, index) {
+    const { mark } = player;
+    console.log(`Placing ${activePlayer.name}'s mark into cell ${mark}...`);
+    gameboard.placeMark(index, player);
+
+    /*  This is where we would check for a winner and handle that logic,
+        such as a win message. */
+    if (checkIfWinner(activePlayer)) printWinner(activePlayer);
+
+    // Switch player turn
+    switchPlayerTurn();
+    printNewRound();
+  };
+
+  return { playRound, getActivePlayer };
 })();
 
-const displayController = (() => {
-  // Methods to render the game state on the UI
+gameController.playRound(gameController.getActivePlayer(), 6);
+gameController.playRound(gameController.getActivePlayer(), 5);
+gameController.playRound(gameController.getActivePlayer(), 8);
+gameController.playRound(gameController.getActivePlayer(), 4);
+gameController.playRound(gameController.getActivePlayer(), 7);
 
-  cells.forEach((cell, i) => {
-    cell.textContent = gameboard.gameboard[i];
-  });
-})();
+// const displayController = (() => {
+//   // Methods to render the game state on the UI
 
-const player = name => {
-  const sayName = () => console.log(`my name is ${name}`);
-  return { sayName };
-};
-
-const checkIfTaken = function (e) {
-  return e.currentTarget.textContent.length > 0;
-};
-
-const handleClick = function (e) {
-  if (!checkIfTaken(e)) e.currentTarget.textContent = 'P';
-};
-
-cells.forEach(cell => cell.addEventListener('click', handleClick));
+//   cells.forEach((cell, i) => {
+//     cell.textContent = gameboard.gameboard[i];
+//   });
+// })();
